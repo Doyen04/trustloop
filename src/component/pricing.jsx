dc9e55b98867cb73fad34d4260052b347cacf0c2
Doyen <101886678+Doyen04@users.noreply.github.com>
2024-11-09 13:13:58 +0100

    added a pricing and subscription component

--- src/component/pricing.jsx
+++ src/component/pricing.jsx
@@ -1,62 +1,118 @@
 import '../styles/pricing.css'
 
-function Pricing(){
-    return(
+import greencheck from '../assets/svg/greencheckedbox.svg'
+import greycheck from '../assets/svg/checkedbox.svg'
+
+function Pricing() {
+    return (
         <div className='pricing'>
             <h1>Pricing</h1>
             <p>Problems trying to resolve the conflict between the two major realms of Classical physics</p>
             <div className='pricings-container'>
                 <div className="pricings">
-                    <h1></h1>
-                    <p></p>
-                    <div>
-                        <h1></h1>
-                        <p></p>
+                    <h1>FREE</h1>
+                    <p>Organize across all apps by hand</p>
+                    <div className='price'>
+                        <p>0</p>
+                        <div className="sub-and-sup">
+                            <p>$</p>
+                            <p>Per Month</p>
+                        </div>
                     </div>
-                    <button></button>
+
                     <div className="plan-benefit">
-                        <img src="" alt="" />
-                        <div className="benefit-description"></div>
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
                     </div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>1GB Cloud storage</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>Email and community support</p>
+                    </div>
+                    <button className='try-for-free'>Try for free</button>
+                </div>
+
+                <div className="pricings">
+                    <section className='floating-tag'>
+                        New
+                    </section>
+                    <h1>Silver</h1>
+                    <p>Organize across all apps by hand</p>
+                    <div className='price'>
+                        <p>9.99</p>
+                        <div className="sub-and-sup">
+                            <p>$</p>
+                            <p>Per Month</p>
+                        </div>
+                    </div>
+
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>1GB Cloud storage</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>Email and community support</p>
+                    </div>
+                    <button className='try-for-free'>Try for free</button>
                 </div>
                 <div className="pricings">
-                    <h1></h1>
-                    <p></p>
-                    <div>
-                        <h1></h1>
-                        <p></p>
+                    <h1>Gold</h1>
+                    <p>Organize across all apps by hand</p>
+                    <div className='price'>
+                        <p>19.99</p>
+                        <div className="sub-and-sup">
+                            <p>$</p>
+                            <p>Per Month</p>
+                        </div>
                     </div>
-                    <button></button>
+
                     <div className="plan-benefit">
-                        <img src="" alt="" />
-                        <div className="benefit-description"></div>
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
                     </div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                </div>
-                <div className="pricings">
-                    <h1></h1>
-                    <p></p>
-                    <div>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <button></button>
                     <div className="plan-benefit">
-                        <img src="" alt="" />
-                        <div className="benefit-description"></div>
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
                     </div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
-                    <div className="plan-benefit"></div>
+                    <div className="plan-benefit">
+                        <img src={greencheck} alt="" />
+                        <p>Unlimited product updates</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>1GB Cloud storage</p>
+                    </div>
+                    <div className="plan-benefit">
+                        <img src={greycheck} alt="" />
+                        <p>Email and community support</p>
+                    </div>
+                    <button className='try-for-free'>Try for free</button>
                 </div>
+
             </div>
         </div>
     )
